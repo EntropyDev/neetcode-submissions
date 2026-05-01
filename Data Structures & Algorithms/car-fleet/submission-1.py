4b64eacf75639ff7cd621c8d242e50d10e31bf72
Vaibhav Chimalgi <vchimu20@gmail.com>
2026-05-01 18:52:11 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        arr = [(p,s) for p,s in zip(position, speed)]
+        st = []
+        for p,s in sorted(arr)[::-1]:
+            st.append((target-p)/s)
+            if len(st) >= 2 and st[-1]<=st[-2]:
+                st.pop()
+        return len(st)
