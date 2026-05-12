d1d1ba81c48994d20e43c2305d6c26725d0cc75c
Vaibhav Chimalgi <vchimu20@gmail.com>
2026-05-12 16:07:10 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(perm, pick):
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return
+            
+            for i in range(len(nums)):
+                if not pick[i]:
+                    perm.append(nums[i])
+                    pick[i] = True
+                    dfs(perm, pick)
+                    perm.pop()
+                    pick[i] = False
+            
+
+
+        dfs([], [False]*len(nums))
+
+        return res
